3c4aac7570be91e47763509afe7091e6f90283bd
vadgonkarmayuresh <vadgonkarmayuresh@gmail.com>
2026-07-04 14:30:02 +0530

    Create p2.py

---
+++ day 12/p2.py
@@ -0,0 +1,8 @@
+class p2:
+    def __init__(self,age):
+        self.age = age
+        print(self.age)   
+    def abc(self):
+        print("From p2 abc")
+    def show(self):
+        print("from p2 show") 
