635ed2175b2193fcf43eeffa527a6e54f57963cc
vadgonkarmayuresh <vadgonkarmayuresh@gmail.com>
2026-07-04 14:29:02 +0530

    Add class p1 with methods and constructor

---
+++ day 12/p1.py
@@ -0,0 +1,8 @@
+class p1:
+    def __init__(self,name):
+        self.name = name
+        print(self.name)        
+    def xyz(self):
+        print("from p1 xyz")
+    def show(self):
+        print("from p1 show")
