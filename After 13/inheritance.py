4bef28ebd2e5f9ca4fca9112337591684e197ab2
vadgonkarmayuresh <vadgonkarmayuresh@gmail.com>
2026-07-21 16:59:55 +0530

    Update inheritance.py

--- After 13/inheritance.py
+++ After 13/inheritance.py
@@ -1,1 +1,24 @@
+class Engine:
+    def __init__(self, cc, horsepower):
+        self.cc = cc
+        self.horsepower = horsepower
 
+    def display_engine(self):
+        return f"{self.cc}, {self.horsepower}"
+
+
+class Car:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        self.engine = Engine("100", "200")
+
+    def display_car(self):
+        return f"{self.name}, {self.price}"
+
+    def display_all(self):
+        return f"{self.display_car()} | {self.engine.display_engine()}"
+
+
+obj = Car("bmw", "1cr")
+print(obj.display_all())
